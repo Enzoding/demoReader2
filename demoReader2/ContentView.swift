bcaecb08c9084d49fa2ffcd55a2ea0973bff4cf3
Enzoding <dxl0805@outlook.com>
2024-07-18 09:06:37 +0800

    Initial Commit

---
+++ demoReader2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demoReader2
+//
+//  Created by 龙龙 on 2024/7/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demoReader2/demoReader2App.swift
@@ -0,0 +1,17 @@
+//
+//  demoReader2App.swift
+//  demoReader2
+//
+//  Created by 龙龙 on 2024/7/18.
+//
+
+import SwiftUI
+
+@main
+struct demoReader2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
